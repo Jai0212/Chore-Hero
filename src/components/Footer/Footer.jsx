176850074b39d2b08f29daac4223e0ea93ec7a50
Jai0212 <jj.joshijai@gmail.com>
2024-06-08 22:10:16 +0400

    changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,34 +3,34 @@ import './Footer.css'
 import { assets } from '../../assets/assets'
 
 const Footer = () => {
-  return (
-    <div className='footer' id='footer'>
-        <div className="footer-content">
-            <div className="footer-content-left">
-                <img src={assets.logo} alt="" className='footer-logo'/>
-                <p>Chore Hero is a fullstack web application wherein users can hire skilled people to do their chores.</p>
+    return (
+        <div className='footer' id='footer'>
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="" className='footer-logo' />
+                    <p>Chore Hero is a fullstack web application wherein users can hire skilled people to do their chores.</p>
+                </div>
+                <div className="footer-content-center">
+                    <h2>INFORMATION</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Our Services</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>CREATOR</h2>
+                    <ul>
+                        <li>Created by Jai Joshi</li>
+                        <li>jj.joshijai@gmail.com</li>
+                    </ul>
+                </div>
             </div>
-            <div className="footer-content-center">
-                <h2>INFORMATION</h2>
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Our Services</li>
-                    <li>Privacy Policy</li>
-                </ul>
-            </div>
-            <div className="footer-content-right">
-                <h2>CREATOR</h2>
-                <ul>
-                    <li>Created by Jai Joshi</li>
-                    <li>jj.joshijai@gmail.com</li>
-                </ul>
-            </div>
+            <hr />
+            <p className="footer-copyright">Copyright 2024 © chore-hero.netlify.app - All Rights Reserved</p>
         </div>
-        <hr />
-        <p className="footer-copyright">Copyright 2024 © chore-hero.com - All Rights Reserved</p>
-    </div>
-  )
+    )
 }
 
 export default Footer
--- src/pages/MyOrders/MyOrders.jsx
+++ src/pages/MyOrders/MyOrders.jsx
@@ -22,32 +22,32 @@ const MyOrders = () => {
 
     return (
         <div className='my-orders'>
-            {data.length === 0 ? 
-            <h2 className='no-services-ordered'>You have not yet ordered any services</h2> : 
-            <div>
-            <h2>My Services</h2>
-            <div className="my-orders-container">
-                {data.map((order, index) => {
-                    return (
-                        <div key={index} className="my-orders-order-container">
-                            <img src={assets.profile_icon} alt="" />
-                            <div className="my-orders-inner">
-                                {order.items.map((item, index) => {
-                                    return (
-                                        <div key={index} className="my-orders-order">
-                                            <h3>{item.name}</h3>
-                                            <p>{item.date}</p>
-                                            <p>{item.time}</p>
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                            <p className='my-orders-cost'>${order.amount}</p>
-                        </div>
-                    )
-                })}
-            </div>
-            </div>}
+            {data.length === 0 ?
+                <h2 className='no-services-ordered'>You have not yet ordered any services</h2> :
+                <div>
+                    <h2>My Services</h2>
+                    <div className="my-orders-container">
+                        {data.map((order, index) => {
+                            return (
+                                <div key={index} className="my-orders-order-container">
+                                    <img src={assets.profile_icon} alt="" />
+                                    <div className="my-orders-inner">
+                                        {order.items.map((item, index) => {
+                                            return (
+                                                <div key={index} className="my-orders-order">
+                                                    <h3>{item.name}</h3>
+                                                    <p>{item.date}</p>
+                                                    <p>{item.time}</p>
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
+                                    <p className='my-orders-cost'>${order.amount}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>}
         </div>
     )
 }
